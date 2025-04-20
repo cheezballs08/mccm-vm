4e5edad89abc7eb496e828c710bb2319e8f2da00
cedar <gamerulzproz@gmail.com>
2025-04-20 23:52:43 +0300

    what

--- target/debug/deps/mccm_emulator-412746d328fb499e.d
+++ target/debug/deps/mccm_emulator-412746d328fb499e.d
@@ -1,6 +1,6 @@
-/home/cedar/Code/rust/mccm-emulator/target/debug/deps/mccm_emulator-412746d328fb499e: src/main.rs src/alu.rs src/decoder.rs src/memory.rs src/cpu.rs src/opcodes/mod.rs src/opcodes/mov.rs src/register.rs
+/home/cedar/Code/rust/mccm-emulator/target/debug/deps/mccm_emulator-412746d328fb499e: src/main.rs src/alu.rs src/decoder.rs src/memory.rs src/cpu.rs src/opcodes/mod.rs src/opcodes/mov.rs src/opcodes/add.rs src/register.rs src/bitops.rs
 
-/home/cedar/Code/rust/mccm-emulator/target/debug/deps/mccm_emulator-412746d328fb499e.d: src/main.rs src/alu.rs src/decoder.rs src/memory.rs src/cpu.rs src/opcodes/mod.rs src/opcodes/mov.rs src/register.rs
+/home/cedar/Code/rust/mccm-emulator/target/debug/deps/mccm_emulator-412746d328fb499e.d: src/main.rs src/alu.rs src/decoder.rs src/memory.rs src/cpu.rs src/opcodes/mod.rs src/opcodes/mov.rs src/opcodes/add.rs src/register.rs src/bitops.rs
 
 src/main.rs:
 src/alu.rs:
@@ -9,4 +9,6 @@ src/memory.rs:
 src/cpu.rs:
 src/opcodes/mod.rs:
 src/opcodes/mov.rs:
+src/opcodes/add.rs:
 src/register.rs:
+src/bitops.rs:
---
+++ target/debug/deps/mccm_emulator-6d27aeed37aaee82.d
@@ -0,0 +1,14 @@
+/home/cedar/Code/rust/mccm-emulator/target/debug/deps/libmccm_emulator-6d27aeed37aaee82.rmeta: src/main.rs src/alu.rs src/decoder.rs src/memory.rs src/cpu.rs src/opcodes/mod.rs src/opcodes/mov.rs src/opcodes/add.rs src/register.rs src/bitops.rs
+
+/home/cedar/Code/rust/mccm-emulator/target/debug/deps/mccm_emulator-6d27aeed37aaee82.d: src/main.rs src/alu.rs src/decoder.rs src/memory.rs src/cpu.rs src/opcodes/mod.rs src/opcodes/mov.rs src/opcodes/add.rs src/register.rs src/bitops.rs
+
+src/main.rs:
+src/alu.rs:
+src/decoder.rs:
+src/memory.rs:
+src/cpu.rs:
+src/opcodes/mod.rs:
+src/opcodes/mov.rs:
+src/opcodes/add.rs:
+src/register.rs:
+src/bitops.rs:
